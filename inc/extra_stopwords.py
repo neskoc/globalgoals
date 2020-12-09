5af1d90c0929b794e83770419f75078cecf5df30
Nenad Cuturic <cnesko@gmail.com>
2020-12-09 13:19:28 +0100

    Stop words sorted alphabeticaly and split in two files

---
+++ inc/extra_stopwords.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 13:08:25 2020
+
+@author: nesko
+"""
+
+
+common_stopwords = [
+    'både',
+    'först',
+    'ge',
+    'minska',
+    'samt',
+    'se',
+    'synnerhet',
+    'sätt',
+    'ta',
+    'vara',
+    'år',
+    'öka'
+    ]
+
+extra_stopwords = [
+    'ansökan', 'avhandling',
+    'bakgrund',
+    'disputationsämne',
+    'huvudsyfte',
+    'målsättning',
+    'konferens',
+    'projekt',
+    'resebidrag',
+    'syfte',
+    'undertecknad'
+    ]
